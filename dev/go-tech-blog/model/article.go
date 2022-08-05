a52326843ae47482b2ec2cfc1cebaaf0ececf067
morimotoyuuki111 <102967614+morimotoyuuki111@users.noreply.github.com>
2022-08-05 15:03:34 +0900

    Add files via upload

--- dev/go-tech-blog/model/article.go
+++ dev/go-tech-blog/model/article.go
@@ -8,11 +8,11 @@ import (
 
 // Article ...
 type Article struct {
- ID      int       `db:"id" form:"id"`
- Title   string    `db:"title" form:"title" validate:"required,max=50"`
- Body    string    `db:"body" form:"body" validate:"required"`
-  Created time.Time `db:"created"`
-  Updated time.Time `db:"updated"`
+  ID      int       `db:"id" form:"id"`
+  Title   string    `db:"title" form:"title" validate:"required,max=50"`
+  Body    string    `db:"body" form:"body" validate:"required"`
+   Created time.Time `db:"created"`
+   Updated time.Time `db:"updated"`
 }
 
 // ValidationErrors ...
